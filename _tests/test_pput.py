de17e61cd44d66230e2c7d60dc4e29fc0f9ecec9
Radu Ciorba <radu@presslabs.com>
2016-01-03 23:38:55 +0200

    refactor and add more tests

--- _tests/test_pput.py
+++ _tests/test_pput.py
@@ -1,56 +1,118 @@
 from cStringIO import StringIO
 from datetime import datetime
+from Queue import Queue
+from uuid import uuid4
+import hashlib
 
+import boto
 import pytest
 
-from z3.pput import Uploader
+from z3.pput import (UploadSupervisor, UploadWorker, StreamHandler,
+                     Result, multipart_etag)
 from z3.config import get_config
 
 
 cfg = get_config()
+_cached_sample_data = None
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture()
 def sample_data():
-    "returns a StringIO with 6 Mbytes"
-    data = StringIO()
-    for count in xrange(6):
-        for _ in xrange(2 * 1024):
-            # each iteration adds 1MB
-            # each 1MB chunk is made up of an alternation of the block's index (zero based)
-            # and an incrementing counter (overflows to 0 several times)
-            # the first block will be: 00 00 00 01 00 02 ... 00 ff 00 00 ... 00 ff
-            data.write(
-                "".join(chr(count)+chr(i) for i in xrange(256))
-            )
-    print "wrote {} MB" .format(data.tell() / 1024.0 / 1024.0)
-    data.seek(0)
-    return data
+    """Sets up a StringIO with 6 Mbytes of data"""
+    global _cached_sample_data
+    if _cached_sample_data is None:
+        data = StringIO()
+        chars = "".join(chr(i) for i in xrange(256))
+        for count in xrange(6):
+            cc = chr(count)
+            for _ in xrange(2 * 1024):
+                # each iteration adds 1MB
+                # each 1MB chunk is made up of an alternation of the block's index (zero based)
+                # and an incrementing counter (overflows to 0 several times)
+                # the first block will be: 00 00 00 01 00 02 ... 00 ff 00 00 ... 00 ff
+                data.write(
+                    "".join(cc+chars[i] for i in xrange(256))
+                )
+        print "wrote {} MB" .format(data.tell() / 1024.0 / 1024.0)
+        _cached_sample_data = data
+    _cached_sample_data.seek(0)
+    return _cached_sample_data
 
 
-def test_multipart_upload(sample_data):
-    uploader = Uploader('presslabstest', cfg['S3_KEY_ID'], cfg['S3_SECRET'])
-    key_name = "z3_test_"+datetime.now().strftime("%Y%m%d_%H-%M-%S")
-    uploader.begin_upload(key_name)
-    index = 1
+def test_multipart_etag(sample_data):
     digests = []
     while True:
         chunk = sample_data.read(5 * 1024 * 1024)
-        print "read {} KB".format(len(chunk) / 1024.0)
         if len(chunk) == 0:
             break
-        # since this is a stringIO there's no way this can return less than 512 bytes
-        # unless we're at the end of the stream
-        print "uploading chunk", index
-        part = uploader.upload_part(
-            uploader.bucket,
-            uploader.multipart.id,
-            uploader.multipart.key_name,
-            chunk,
-            index)
-        digests.append(part.md5)
-        index += 1
-    uploader.finish_upload()
-    expected_etag = uploader.multipart_etag(digests)
-    actual_etag = uploader.bucket.get_key(key_name).etag
-    assert expected_etag == actual_etag
+        digest = hashlib.md5()
+        digest.update(chunk)
+        digests.append(digest.hexdigest())
+    assert multipart_etag(digests) == '"d229c1fc0e509475afe56426c89d2724-2"'
+
+
+def test_stream_handler():
+    stream_handler = StreamHandler(StringIO("aabbccdde"), chunk_size=2)
+    chunks = []
+    while not stream_handler.finished:
+        chunk = stream_handler.get_chunk()
+        chunks.append(chunk)
+    assert chunks == ['aa', 'bb', 'cc', 'dd', 'e']
+
+
+def test_handle_results():
+    sup = UploadSupervisor(None, None, None)
+    sup.inbox = Queue()
+    sup._pending_chunks = 3
+    sup.inbox.put(Result(success=True, traceback=None, index=1, md5='a'))
+    sup.inbox.put(Result(success=True, traceback=None, index=3, md5='c'))
+    sup.inbox.put(Result(success=True, traceback=None, index=2, md5='b'))
+    sup._handle_results()
+    assert sorted(sup.results) == [(1, 'a'), (2, 'b'), (3, 'c')]
+    assert sup._pending_chunks == 0
+
+
+class FakeMultipart(object):
+    def __init__(self, name):
+        self._name = name
+        self.id = str(uuid4())
+        self.key_name = str(uuid4())
+        self._completed = False
+
+    def complete_upload(self):
+        if self._completed:
+            raise AssertionError('multipart already completed')
+        self._completed = True
+
+
+class FakeBucket(object):
+    def __init__(self):
+        self._multipart = None
+
+    def initiate_multipart_upload(self, name, headers):
+        self._multipart = FakeMultipart(name)
+        return self._multipart
+
+
+class DummyWorker(UploadWorker):
+    def upload_part(self, index, chunk):
+        return hashlib.md5(chunk).hexdigest()
+
+
+def test_supervisor_loop(sample_data):
+    stream_handler = StreamHandler(sample_data)
+    bucket = FakeBucket()
+    sup = UploadSupervisor(stream_handler, 'test', bucket=bucket)
+    etag = sup.main_loop(worker_class=DummyWorker)
+    assert etag == '"d229c1fc0e509475afe56426c89d2724-2"'
+    assert bucket._multipart._completed
+
+
+def test_integration(sample_data):
+    stream_handler = StreamHandler(sample_data)
+    bucket = boto.connect_s3(
+        cfg['S3_KEY_ID'], cfg['S3_SECRET']).get_bucket('presslabstest')
+    key_name = "z3_test_"+datetime.now().strftime("%Y%m%d_%H-%M-%S")
+    sup = UploadSupervisor(stream_handler, key_name, bucket=bucket)
+    etag = sup.main_loop()
+    assert etag == '"d229c1fc0e509475afe56426c89d2724-2"'
--- z3/pput.py
+++ z3/pput.py
@@ -6,63 +6,194 @@ pput bucket_name/filename
 
 import binascii
 import hashlib
+import functools
+from collections import namedtuple
 from cStringIO import StringIO
+from Queue import Queue
+from logging import getLogger
+from threading import Thread
 
-import boto
+import boto.s3.multipart
+
+
+from z3.config import get_config
+
+
+Result = namedtuple('Result', ['success', 'traceback', 'index', 'md5'])
+CFG = get_config()
+
+
+def multipart_etag(digests):
+    """
+    Computes etag for multipart uploads
+    :type digests: list of hex-encoded md5 sums (string)
+    :param digests: The list of digests for each individual chunk.
+
+    :rtype: string
+    :returns: The etag computed from the individual chunks.
+    """
+    etag = hashlib.md5()
+    count = 0
+    for dig in digests:
+        count += 1
+        etag.update(binascii.a2b_hex(dig))
+    return '"{}-{}"'.format(etag.hexdigest(), count)
 
 
 class StreamHandler(object):
-    '''Reads chunks and dispatches them to an Uploader'''
-    def __init__(self, uploader, chunk_size=512, concurrency=5):
-        self.uploader = uploader
+    def __init__(self, input_stream, chunk_size=5*1024*1024):
+        self.input_stream = input_stream
         self.chunk_size = chunk_size
-        self.concurrency = concurrency
+        self._partial_chunk = ""
+        self._eof_reached = False
 
-    def upload_stream(self, stream):
-        while stream.read(self.chunk_size):
-            pass
+    @property
+    def finished(self):
+        return self._eof_reached and len(self._partial_chunk) == 0
+
+    def get_chunk(self):
+        """Return complete chunks or None if EOF reached"""
+        while not self._eof_reached:
+            read = self.input_stream.read(self.chunk_size - len(self._partial_chunk))
+            if len(read) == 0:
+                self._eof_reached = True
+            self._partial_chunk += read
+            if len(self._partial_chunk) == self.chunk_size or self._eof_reached:
+                chunk = self._partial_chunk
+                self._partial_chunk = ""
+                return chunk
 
 
-class Uploader(object):
-    def __init__(self, bucket_name, key_id, secret):
-        self.bucket_name = bucket_name
-        self.key_id = key_id
-        self.secret = secret
-        self.connection = boto.connect_s3(self.key_id, self.secret)
-        self.bucket = self.connection.get_bucket(self.bucket_name, validate=True)
+@functools.wraps
+def retry(func):
+    def wrapped(*a, **kwa):
+        times = int(CFG['max_retries'])
+        for attempt in xrange(times):
+            try:
+                return func(*a, **kwa)
+            except: # pylint: disable=bare-except
+                if attempt+1 >= times:
+                    raise
+    return wrapped
+
+
+class UploadWorker(object):
+    def __init__(self, bucket, multipart, inbox, outbox):
+        self.bucket = bucket
+        self.inbox = inbox
+        self.outbox = outbox
+        self.multipart = multipart
+        self._thread = None
+        self.log = getLogger('UploadWorker')
+
+    @retry
+    def upload_part(self, index, chunk):
+        part = boto.s3.multipart.MultiPartUpload(self.bucket)
+        part.id = self.multipart.id
+        part.key_name = self.multipart.key_name
+        return part.upload_part_from_file(
+            StringIO(chunk), index, replace=True).md5
+
+    def start(self):
+        self._thread = Thread(target=self.main_loop)
+        self._thread.daemon = True
+        self._thread.start()
+        return self
+
+    def main_loop(self):
+        while True:
+            index, chunk = self.inbox.get()
+            md5 = self.upload_part(index, chunk)
+            # print "worker loop i:{} md5:{}".format(index, md5)
+            self.outbox.put(Result(
+                success=True,
+                md5=md5,
+                traceback=None,
+                index=index,
+            ))
+
+
+class UploadSupervisor(object):
+    '''Reads chunks and dispatches them to UploadWorkers'''
+
+    def __init__(self, stream_handler, name, bucket):
+        self.stream_handler = stream_handler
+        self.name = name
+        self.bucket = bucket
+        self.inbox = None
+        self.outbox = None
         self.multipart = None
+        self.results = []  # beware s3 multipart indexes are 1 based
+        self._pending_chunks = 0
 
-    def begin_upload(self, name):
+    def _start_workers(self, concurrency, worker_class):
+        work_queue = Queue(maxsize=concurrency)
+        result_queue = Queue()
+        self.outbox = work_queue
+        self.inbox = result_queue
+        workers = [
+            worker_class(
+                bucket=self.bucket,
+                multipart=self.multipart,
+                inbox=work_queue,
+                outbox=result_queue,
+            ).start()
+            for _ in xrange(concurrency)]
+        return workers
+
+    def _begin_upload(self):
         if self.multipart is not None:
             raise AssertionError("multipart upload already started")
         self.multipart = self.bucket.initiate_multipart_upload(
-            name,
+            self.name,
             headers={
                 "x-amz-acl": "bucket-owner-full-control",
             }
         )
 
-    @staticmethod
-    def upload_part(bucket, part_id, key_name, chunk, index):
-        part = boto.s3.multipart.MultiPartUpload(bucket)
-        part.id = part_id
-        part.key_name = key_name
-        return part.upload_part_from_file(StringIO(chunk), index, replace=True)
-
-    @staticmethod
-    def multipart_etag(digests):
-        """
-        Computes etag for multipart uploads
-        :type digests: list of hex-encoded md5 sums (string)
-        :param digests: The list of digests for each individual chunk.
-
-        :rtype: string
-        :returns: The etag computed from the individual chunks.
-        """
-        etag = hashlib.md5()
-        for dig in digests:
-            etag.update(binascii.a2b_hex(dig))
-        return '"{}-{}"'.format(etag.hexdigest(), len(digests))
-
-    def finish_upload(self):
+    def _finish_upload(self):
         return self.multipart.complete_upload()
+
+    def _handle_result(self):
+        """Process one result. Block untill one is available
+        """
+        result = self.inbox.get()
+        if result.success:
+            self.results.append((result.index, result.md5))
+            self._pending_chunks -= 1
+        else:
+            raise result.traceback
+
+    def _handle_results(self):
+        """Process any available result
+        Doesn't block.
+        """
+        while not self.inbox.empty():
+            self._handle_result()
+
+    def _send_chunk(self, index, chunk):
+        """Send the current chunk to the workers for processing.
+        Called when the _partial_chunk is complete.
+
+        Blocks when the outbox is full.
+        """
+        self._pending_chunks += 1
+        self.outbox.put((index, chunk))
+
+    def main_loop(self, concurrency=4, worker_class=UploadWorker):
+        chunk_index = 0
+        self._begin_upload()
+        self._start_workers(concurrency, worker_class=worker_class)
+        while self._pending_chunks or not self.stream_handler.finished:
+            # print "main_loop p:{} o:{} i:{}".format(
+            #     self._pending_chunks, self.outbox.qsize(), self.inbox.qsize())
+            # consume results first as this is a quick operation
+            self._handle_results()
+            chunk = self.stream_handler.get_chunk()
+            if chunk:
+                # s3 multipart index is 1 based, increment before sending
+                chunk_index += 1
+                self._send_chunk(chunk_index, chunk)
+        self._finish_upload()
+        self.results.sort()
+        return multipart_etag(r[1] for r in self.results)
