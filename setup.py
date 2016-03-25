7cf66f1f066d639227c3dffa3de31f5de92483a2
Radu Ciorba <radu@presslabs.com>
2016-03-25 17:56:15 +0200

    bump version to 0.1.10

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="z3",
-    version="0.1.9",
+    version="0.1.10",
     platforms='any',
     packages=find_packages(),
     include_package_data=True,
