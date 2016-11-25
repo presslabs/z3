403b1647cfe6946137d2140ec57c7ce77a4b3c25
Vlad Temian <vladtemian@gmail.com>
2016-11-25 09:00:27 +0200

    Bump version to 0.1.14

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="z3",
-    version="0.1.13",
+    version="0.1.14",
     platforms='any',
     packages=find_packages(),
     include_package_data=True,
