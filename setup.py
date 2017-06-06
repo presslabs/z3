033b69a4688e64559ecfc4da0e40e6adc98594dc
Radu Ciorba <radu@devrandom.ro>
2017-06-06 19:30:40 +0300

    bump version to 0.1.15

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="z3",
-    version="0.1.14",
+    version="0.1.15",
     platforms='any',
     packages=find_packages(),
     include_package_data=True,
