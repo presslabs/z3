b66375deb2b2732bf71feb4352060808f3fb29dd
Vlad Temian <vladtemian@gmail.com>
2018-03-06 13:00:13 +0200

    Bump version 0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="z3",
-    version="0.1.15",
+    version="0.2.0",
     platforms='any',
     packages=find_packages(),
     include_package_data=True,
